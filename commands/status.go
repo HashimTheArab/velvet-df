d0e6c5a286e680f86c0d97c74f3f71c84c9d334b
Prim69 <13991048+Prim69@users.noreply.github.com>
2022-01-23 23:09:46 -0500

    status command

--- commands/status.go
+++ commands/status.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/df-mc/dragonfly/server/cmd"
@@ -9,6 +8,7 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"time"
 	"velvet/utils"
 )
@@ -17,11 +17,12 @@ type Status struct{}
 
 type statusData struct {
 	memory struct {
-		Total string `json:"MemTotal"`
-		Free  string `json:"MemAvailable"`
+		Free  string
+		Used  string
+		Total string
 	}
 	cpu struct {
-		Model string `json:"model name"`
+		Model string
 	}
 }
 
@@ -31,12 +32,12 @@ func (Status) Run(_ cmd.Source, output *cmd.Output) {
 		add := func(name, value string) {
 			status += "§d" + name + ": §e" + value + "\n"
 		}
-
 		s := getStatusData()
+		s.format()
 
 		add("Uptime", (time.Second * time.Duration(time.Now().Unix()-utils.Started)).String())
-		add("CPU Usage", "")
-		add("Memory", fmt.Sprintf("§d(§c%v§d/§a%v", s.memory.Free, s.memory.Total))
+		add("CPU Model", s.cpu.Model)
+		add("Memory", fmt.Sprintf("§d(§c%v§d/§a%v", s.memory.Used, s.memory.Total))
 		for _, w := range utils.WorldMG.Worlds() {
 			players := 0
 			for _, v := range w.Entities() {
@@ -59,13 +60,49 @@ func getStatusData() statusData {
 	switch runtime.GOOS {
 	case "linux", "android":
 		if d, err := os.ReadFile("/proc/meminfo"); err == nil {
-			_ = json.Unmarshal(d, &s.memory)
+			parseFields(d, map[string]*string{"MemAvailable": &s.memory.Free, "MemTotal": &s.memory.Total}, true)
 		}
 		if d, err := os.ReadFile("/proc/cpuinfo"); err == nil {
-			_ = json.Unmarshal(d, &s.cpu)
+			parseFields(d, map[string]*string{"model name": &s.cpu.Model}, false)
 		}
 	}
 	return s
+}
+
+func parseFields(data []byte, fields map[string]*string, memory bool) {
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		index := strings.IndexRune(line, ':')
+		if index == -1 {
+			continue
+		}
+		if val, ok := fields[line[:index]]; ok {
+			if memory {
+				*val = strings.TrimSpace(strings.TrimSuffix(line[:index+1], "kB"))
+			} else {
+				// todo
+			}
+		}
+	}
+}
+
+func (d *statusData) format() {
+	var memory []int
+	formatStorage := func(s *string) {
+		if n, err := strconv.Atoi(*s); err == nil {
+			*s = strconv.Itoa(n*1024) + "MB"
+			memory = append(memory, n*1024)
+		} else {
+			*s = "Unavailable"
+		}
+	}
+	formatStorage(&d.memory.Free)
+	formatStorage(&d.memory.Total)
+	if len(memory) >= 2 {
+		d.memory.Used = strconv.Itoa(memory[1]-memory[0]) + "MB"
+	} else {
+		d.memory.Used = "Unavailable"
+	}
 }
 
 func (Status) Allow(s cmd.Source) bool { return checkStaff(s) }
