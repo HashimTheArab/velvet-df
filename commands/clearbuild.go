e4e1fbcd4725daf427d210130ba1e24bcb371d54
Hashim <13991048+Prim69@users.noreply.github.com>
2022-02-06 03:11:15 -0500

    clear build on another thread

--- commands/clearbuild.go
+++ commands/clearbuild.go
@@ -10,18 +10,20 @@ import (
 type ClearBuild struct{}
 
 func (t ClearBuild) Run(_ cmd.Source, output *cmd.Output) {
-	start := time.Now()
-	if w, ok := utils.WorldMG.World(utils.Config.World.Build); ok {
-		utils.BuildBlocks.Mutex.Lock()
-		for pos := range utils.BuildBlocks.Blocks {
-			w.SetBlock(pos, block.Air{})
-			delete(utils.BuildBlocks.Blocks, pos)
+	go func() {
+		start := time.Now()
+		if w, ok := utils.WorldMG.World(utils.Config.World.Build); ok {
+			utils.BuildBlocks.Mutex.Lock()
+			for pos := range utils.BuildBlocks.Blocks {
+				w.SetBlock(pos, block.Air{})
+				delete(utils.BuildBlocks.Blocks, pos)
+			}
+			utils.BuildBlocks.Mutex.Unlock()
+			output.Printf("§dBuild was cleared in §e%v.", time.Now().Sub(start).Round(time.Millisecond*10).String())
+		} else {
+			output.Printf("§cBuild is currently offline.")
 		}
-		utils.BuildBlocks.Mutex.Unlock()
-		output.Printf("§dBuild was cleared in §e%v.", time.Now().Sub(start).Round(time.Millisecond*10).String())
-	} else {
-		output.Printf("§cBuild is currently offline.")
-	}
+	}()
 }
 
 func (ClearBuild) Allow(s cmd.Source) bool { return checkStaff(s) || checkConsole(s) }
