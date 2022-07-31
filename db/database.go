fb9ebd8322819e6584b5bb9f725b995b55458d1f
Hashim <13991048+Prim69@users.noreply.github.com>
2022-07-31 13:52:24 -0400

    db

--- db/database.go
+++ db/database.go
@@ -3,22 +3,23 @@ package db
 import (
 	"github.com/upper/db/v4"
 	"github.com/upper/db/v4/adapter/mongo"
+	"runtime"
 )
 
 var sess db.Session
 
 // init creates the database connection.
 func init() {
-	const (
-		localHost  = "127.0.0.1:27017"
-		normalHost = "velvetpractice.tk:26919"
-	)
+	host := "velvetpractice.tk:26919"
+	if runtime.GOOS == "linux" {
+		host = "127.0.0.1:27017"
+	}
 
 	var err error
 	if sess, err = mongo.Open(mongo.ConnectionURL{
 		User:     "Hashim",
 		Password: "9AHn2GahV2IXJWHTr80f6dozWEzKMiks3",
-		Host:     normalHost,
+		Host:     host,
 		Database: "velvet",
 		Options: map[string]string{
 			"authSource":    "admin",
